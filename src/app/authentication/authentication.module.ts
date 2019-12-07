5f6270389aef93b13bb8a3c458c090929bc3b927
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-12-07 08:25:59 +0200

    Added register component.

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './authentication.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AuthenticationComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
