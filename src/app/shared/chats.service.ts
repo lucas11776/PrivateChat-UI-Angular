884f062139ffd86bc7bdadf072d4579997841295
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-12-19 10:15:21 +0200

    Added date service.

--- src/app/shared/chats.service.ts
+++ src/app/shared/chats.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,6 @@ import { Injectable } from '@angular/core';
 export class ChatsService {
 
   constructor() { }
+
+  
 }
