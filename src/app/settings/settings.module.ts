433bebfd5afd71312e3d67db9551ccc5e83962b3
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-12-24 09:31:01 +0200

    Added

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
 import { ChangeProfilePictureComponent } from './change-profile-picture/change-profile-picture.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 
 @NgModule({
   declarations: [
     SettingsComponent,
-    ChangeProfilePictureComponent
+    ChangeProfilePictureComponent,
+    ChangePasswordComponent
   ],
   imports: [
     CommonModule,
